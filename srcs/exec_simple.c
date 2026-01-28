ae93ce673e5c4049e1e648f43e173f9a77a56462
mesalman42 <mervesalman_07@hotmail.com>
2026-01-28 20:00:33 +0300

    Revert "notlarım"
    This reverts commit b385c5a9ea10b9f0a7c1259df61829182b42e3d3.

--- srcs/exec_simple.c
+++ srcs/exec_simple.c
@@ -17,33 +17,22 @@ char *ms_find_path(char *cmd, char **envp)
     int     i;
     char    **paths;
     char    *final_path;
-
-
     if (ft_strchr(cmd, '/'))
         return (ft_strdup(cmd));
-    ///bin/ls ./a.out yazsaydı:
-    //PATH’e bakılmaz
-    //“Bu yolu dene” denirdi
-    Ama "ls" → slash yok, devam.
 
     i = 0;
     while (envp[i] && ft_strncmp(envp[i], "PATH=", 5) != 0)
         i++;
-    //“PATH= ile başlayan env satırını bul”
     if (!envp[i]) return (NULL);
 
     paths = ft_split(envp[i] + 5, ':');
-    //"/usr/bin:/bin"
-    //→ ["/usr/bin", "/bin", NULL]
     if (!paths) return (NULL);
 
     i = 0;
     while (paths[i])
     {
         final_path = join_path(paths[i], cmd);
-        //command ile pathi birleştirir
-        // /usr/bin/ls
-        if (access(final_path, X_OK) == 0) //dosya var mı ve çalıştırılabilir mi?
+        if (access(final_path, X_OK) == 0)
         {
             free_tab(paths);
             return (final_path);
@@ -62,47 +51,42 @@ void ms_exec_simple(t_ctx *ctx, char **argv, char **envp)
     int     status;
 
     path = ms_find_path(argv[0], envp);
-    //komutun yolu bulunur 
-    if (!path) //komut bulunamazsa
+    if (!path)
     {
         write(2, "minishell: ", 11);
         write(2, argv[0], ft_strlen(argv[0]));
         write(2, ": command not found\n", 20);
-        ctx->last_status = 127; //komut yok exit statusu
+        ctx->last_status = 127;
         return ;
     }
 
     pid = fork();
-    //process koopyalanır
-    if (pid == 0) // child process içindeyiz
+    if (pid == 0)
     {
 
-        if (execve(path, argv, envp) == -1) //ls komutu execve olmazsa yani çalışmazsa perror 
+        if (execve(path, argv, envp) == -1)
         {
             perror("minishell"); 
             free(path); 
-            _exit(126); //komut var ama çalıştırılamıyor exiti
+            _exit(126); 
         }
     }
-    else if (pid > 0) //shell köşede bekler
+    else if (pid > 0)
     {
-        waitpid(pid, &status, 0); //şunla bekler aslında
-        while (WIFEXITED(status) == 0 && WIFSIGNALED(status) == 0) 
+        waitpid(pid, &status, 0);
+        while (WIFEXITED(status) == 0 && WIFSIGNALED(status) == 0)
         {
              if (waitpid(pid, &status, 0) == -1 && errno != EINTR)
                 break; 
         }
 
-        if (WIFEXITED(status)) //normal mi öldü
+        if (WIFEXITED(status))
             ctx->last_status = WEXITSTATUS(status);
-        else if (WIFSIGNALED(status)) // signal ile mi öldü
+        else if (WIFSIGNALED(status))
              ctx->last_status = 128 + WTERMSIG(status); 
         
         free(path);
     }
     else
-        perror("fork"); //fork gerçekleşmeme durumu
-    //perror kernelden gelen hata mesajını yazar
-    // fork : kernel mesajı gibi 
-    
+        perror("fork");
 }
--- srcs/main.c
+++ srcs/main.c
@@ -2,21 +2,15 @@
 volatile sig_atomic_t g_sig = 0;
 
 void handle_sigint(int sig)
-//ctrl c kısmında ne yapılacağı  ele alınmış
 {
     (void)sig;
     write(1, "\n", 1);
     
     rl_on_new_line();
-    //imleç yeni satırda
     rl_replace_line("", 0);
-    //halihazırda yazılmış komut satırını sil
     rl_redisplay();
-    //boş satırı göster
 
-    g_sig = SIGINT;
-    //last status güncellensin diye
-    //last_status, shell’in en son çalıştırdığı komutun nasıl bittiğini tutar. $? ile bakılabilir
+    g_sig = SIGINT; 
 }
 
 void ms_loop(t_ctx *ctx, char **envp)
@@ -25,22 +19,16 @@ void ms_loop(t_ctx *ctx, char **envp)
     char **argv;
 
     while(1)
-    //shelll sonsuz döngüde çalışacak
     {
         if (g_sig == SIGINT)
         {
             ctx->last_status = 130;
-            // 0-128 normal exit, 128+ signal kaynaklı exit
-            //SIGINT = 2 , SIGQUIT = 3, SIGKILL = 9, SIGSEGV = 11...
             g_sig = 0;
         }
 
         line = readline("minishell$ ");
-        //ekrana prompt basar, kullanıcıdan bir satır yazı bekler
 
         if(line == NULL)
-        //ctrl+d
-        //terminal kapanır, stdin e EOF gönderilir
         {
             if(ctx->interactive)
                 write(STDERR_FILENO, "exit\n", 5);
@@ -48,31 +36,27 @@ void ms_loop(t_ctx *ctx, char **envp)
         }   
 
         if(line[0] == '\0')
-        //terminalde bi şey yapmadan sadece enter a basma durumu
         {
             free(line);
             continue;
         }
         add_history(line);
-        //istisnai durumlar yoksa line ı history e ekler
 
         if (ft_strncmp(line, "$?", 3) == 0)
-        //$? last statusu kontrol etmemizi sağlayan prompt
         {
             printf("%d\n", ctx->last_status);
             free(line);
             continue;
         }
+
         argv = ft_split(line, ' ');
-        //lexer kısmı 
-        // token list oluşturmayı sağlar
         if (argv)
         {
-            if (argv[0]) // en az bir argüman varsa
-                ms_exec_simple(ctx, argv, envp); //çalıştırılacak
-            free_tab(argv);//token list temizle
+            if (argv[0]) 
+                ms_exec_simple(ctx, argv, envp);
+            free_tab(argv);
         }
-        free(line);//prompt satırını temizle
+        free(line);
     }
 }
    
@@ -84,14 +68,12 @@ int main (int ac, char **av, char **envp)
     (void)av;
     
     signal(SIGINT, handle_sigint);
-    //ctrl c gelince ne tetiklenicek
     signal(SIGQUIT, SIG_IGN);
-    // Ctrl+\ 'i yoksay
     
-    if(ms_ctx_init(&ctx, envp) == false)   // Context'i başlat
-        return 1; // Başarısızsa çık
+    if(ms_ctx_init(&ctx, envp) == false)
+        return 1;
 
-    ms_loop(&ctx, envp); // Ana döngüyü başlat
+    ms_loop(&ctx, envp);
 
-    return (ctx.last_status);   // Son komutun exit kodu ile çık
+    return (ctx.last_status);
 }
